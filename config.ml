49a5ad82a8c3caabad0f9a478b8f337913f42679
Leon Vack <dev@lgcl.de>
2023-01-03 20:12:20 +0100

    pin ocaml < 5.0.0

--- config.ml
+++ config.ml
@@ -1,4 +1,4 @@
 open Mirage
 
-let main = main ~packages:[ package "duration" ] "Unikernel.Hello" (time @-> job)
+let main = main ~packages:[ package "ocaml" ~max:"5.0.0"; package "duration" ] "Unikernel.Hello" (time @-> job)
 let () = register "hello" [ main $ default_time ]
